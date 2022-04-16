c142375efc1347bf635a9362554b682cf6666f08
Christian Köberl <christian.koeberl@porscheinformatik.com>
2022-04-16 22:59:43 +0200

    Fix deploy script

--- functions/deploy.sh
+++ functions/deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
+set -euo pipefail
 
-FUNCAPPNAME=`az deployment group create --resource-group=$1 --name azure-functions --template-file azure-functions.bicep --query properties.outputs.functionAppName.value`
+FUNCAPPNAME=$(az deployment group create --resource-group=$1 --name azure-functions --template-file azure-functions.bicep --query properties.outputs --output json | jq -r .functionAppName.value)
 
 func azure functionapp publish $FUNCAPPNAME
-
